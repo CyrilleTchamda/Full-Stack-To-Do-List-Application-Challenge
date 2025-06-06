e6fc0a5ae23e9afe0746df7b39e23e73d5dd31d1
CyrilleTchamda <cyrilletchamda@gmail.com>
2025-06-06 20:14:01 +0100

    Add initial database and server setup

---
+++ BACKEND/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express()
+
+// middleware
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+
+
+
+
+
+
+
+
+// routers
+
+
+
+
+
+//port
+
+const PORT = process.env.PORT || 8080
+
+//server
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
